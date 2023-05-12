afc331d2c63165c2363375beef63c01b4669294e
taraswww777 <taraswww777@mail.ru>
2023-05-12 18:01:40 +0300

    add antd theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import './App.css'
 import {ConfigProvider} from 'antd';
 import {ANT_THEME_CONFIG} from 'src/constants/configTheme.ts';
+<<<<<<< HEAD
 import {CommonLayout} from 'src/components/CommonLayout';
+<<<<<<< HEAD
 import {Calculator} from 'src/components/Calculator';
+=======
+=======
+>>>>>>> 3748f2c (add antd theme)
+>>>>>>> 7f4dcda (add antd theme)
 
 function App() {
 
   return (
     <ConfigProvider theme={ANT_THEME_CONFIG}>
+<<<<<<< HEAD
       <CommonLayout>
         <Calculator />
       </CommonLayout>
+=======
+<<<<<<< HEAD
+      <CommonLayout />
+=======
+
+>>>>>>> 3748f2c (add antd theme)
+>>>>>>> 7f4dcda (add antd theme)
     </ConfigProvider>
   )
 }
--- src/constants/configTheme.ts
+++ src/constants/configTheme.ts
@@ -1,5 +1,14 @@
+<<<<<<< HEAD
 import {theme, ThemeConfig} from 'antd';
 
 export const ANT_THEME_CONFIG: ThemeConfig = {
   algorithm: theme.darkAlgorithm,
+=======
+import {ThemeConfig} from 'antd';
+
+export const ANT_THEME_CONFIG: ThemeConfig = {
+  token: {
+    colorPrimary: '#00b96b',
+  },
+>>>>>>> 3748f2c (add antd theme)
 }
